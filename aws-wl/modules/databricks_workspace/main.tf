7e9122792e491acda611a1cfdcf63d8584803195
JDBraun <JDBraun@gmail.com>
2023-02-23 15:00:50 -0600

    Premium pricing tier update

--- aws-wl/modules/databricks_workspace/main.tf
+++ aws-wl/modules/databricks_workspace/main.tf
@@ -41,7 +41,7 @@ resource "databricks_mws_workspaces" "this" {
   credentials_id                           = databricks_mws_credentials.this.credentials_id
   storage_configuration_id                 = databricks_mws_storage_configurations.this.storage_configuration_id
   network_id                               = databricks_mws_networks.this.network_id
-  pricing_tier                             = "ENTERPRISE"
+  pricing_tier                             = "PREMIUM"
   depends_on                               = [databricks_mws_networks.this]
   external_customer_info {
     customer_name = var.customer_name 
